1a832081d884601d32a646e27475f7217819d32c
unknown <zhyan0096@126.com>
2021-07-14 20:02:54 +0800

    third commit

--- venv/Include/text_operation/test006/http_try.py
+++ venv/Include/text_operation/test006/http_try.py
@@ -5,11 +5,13 @@
 from PyQt5 import uic
 from PyQt5.QtCore import QFile
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QIcon
 class HTTP:
     def __init__(self):
-        self.ui = uic.loadUi(r"C:\Users\Administrator\Desktop\qss_pilot2\http.ui")
+        self.ui = uic.loadUi(r".\http.ui")
 
 app = QApplication([])
+app.setWindowIcon(QIcon("icons8-aliexpress-100.png"))
 http = HTTP()
 http.ui.show()
 app.exec_()
--- venv/Include/text_operation/test006/text_editor.py
+++ venv/Include/text_operation/test006/text_editor.py
@@ -3,42 +3,51 @@
 # @Author: little kimber
 # @File: text_editor.py
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QPlainTextEdit, QMessageBox
+from PyQt5.QtWidgets import QFileDialog, QColorDialog, QFontDialog
+from PyQt5 import uic
+from PyQt5.QtCore import QFile
+from PyQt5.QtGui import QIcon
+import sys
 
 
 class Editor():
     def __init__(self):
-        # 初始化界面
-        self.window = QMainWindow()
-        self.window.resize(600, 300)
-        self.window.move(310, 300)
-        self.window.setWindowTitle("阿庄文本编辑器")
+        self.ui = uic.loadUi("text_editor.ui")
+        self.ui.open_button.clicked.connect(self.open_file)
+        self.ui.save_button.clicked.connect(self.save_file)
+        self.ui.clear_button.clicked.connect(self.clear_file)
+        self.ui.font_button.clicked.connect(self.font_change)
+        self.ui.color_button.clicked.connect(self.color_change)
 
-        self.textEdit = QPlainTextEdit(self.window)
-        self.textEdit.setPlaceholderText("请输入文本")
-        self.textEdit.move(20, 50)
-        self.textEdit.resize(400, 200)
+    def open_file(self):
+        files = QFileDialog.getOpenFileName(self.ui, "打开本地文件")
+        if files[0]:
+            with open(files[0], mode='r', encoding="utf-8") as f:
+                self.ui.text.setText(f.read())
 
-        self.button1 = QPushButton("打开", self.window)
-        self.button1.clicked.connect(Open)
+    def save_file(self):
+        file = QFileDialog.getSaveFileName(self.ui, "保存文件")
+        if file[0]:
+            print(file[0])
+            with open(file[0], "w") as f:
+                f.write(self.ui.text.toPlainText())
 
-        self.button2 = QPushButton("编辑", self.window)
-        self.button2.move(120, 0)
-        self.button2.clicked.connect(Edit)
+    def clear_file(self):
+        self.ui.text.clear()
 
-        self.button3 = QPushButton("保存", self.window)
-        self.button3.move(240, 0)
-        self.button3.clicked.connect(Save)
+    def font_change(self):
+        fo, b = QFontDialog.getFont()
+        if b:
+            self.ui.text.setCurrentFont(fo)
 
-    def Open(self):
-        pass
+    def color_change(self):
+        co = QColorDialog.getColor()
+        if co.isValid():
+            self.ui.text.setTextColor(co)
 
-    def Edit(self):
-        pass
 
-    def Save(self):
-        pass
 
-app = QApplication([])
+app = QApplication(sys.argv)
 editor = Editor()
-editor.window.show()
+editor.ui.show()
 app.exec_()
